aab524ff4194ee0afd21ce5a009d644987659146
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-07-01 21:31:04 +0300

    Ex2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Example1");
         for(int i = 0; i <= 10; i++ )
         {
             System.out.println(i);
+        }
+        System.out.println("Example2");
+        for(int a = 10; a >= 1; a--)
+        {
+            System.out.println(a);
         }
     }
 }
